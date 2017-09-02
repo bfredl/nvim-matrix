58601ed463e830dc88c3cab80900755b90611543
Björn Linse <bjorn.linse@gmail.com>
2017-09-02 20:54:19 +0200

    wörk

--- rplugin/python3/matrix/__init__.py
+++ rplugin/python3/matrix/__init__.py
@@ -1,13 +1,38 @@
 import neovim
 from matrix_client.client import MatrixClient
 from functools import partial
+import re
+from collections import deque
 
 class obj:
     pass
 self = obj()
 
+class ExclusiveHandler(object):
+    """Wrapper for buffering incoming messages from a asynchronous source.
+
+    Wraps an async message handler function and ensures a previous message will
+    be completely handled before next messsage is processed. Is used to avoid
+    iopub messages being printed out-of-order or even interleaved.
+    """
+    def __init__(self, handler):
+        self.msgs = deque()
+        self.handler = handler
+        self.is_active = False
+
+    def __call__(self, *args):
+        self.msgs.append(args)
+        if not self.is_active:
+            self.is_active = True
+            while self.msgs:
+                self.handler(*self.msgs.popleft())
+            self.is_active = False
+
+re_gitter = r"@gitter_([^:]+):matrix.org"
+re_freenode = r"@freenode_([^:]+):matrix.org"
+re_matrix = re.compile(r"@([^:]+):matrix.org")
+
 @neovim.plugin
-@neovim.encoding(True)
 class IPythonPlugin(object):
     def __init__(self, vim):
         self.vim = vim
@@ -29,11 +54,9 @@ class IPythonPlugin(object):
 
     def buf_write(self, data):
         #self.hl_handler.reset_sgr()
-        lineidx = len(self.buf)-1
-        lastline = self.buf[-1]
+        lineidx = len(self.buf)
 
-        txt = lastline + data
-        self.buf[-1:] = txt.split("\n") # not splitlines
+        self.buf.append(data.split("\n")) # not splitlines
 
         for w in self.vim.windows:
             if w.buffer == self.buf and w != self.vim.current.window:
@@ -41,15 +64,34 @@ class IPythonPlugin(object):
 
         return lineidx
 
+    def format_sender(self, sender):
+        m = re_matrix.match(sender)
+        if not m:
+            return sender, None
+        name = m.group(1)
+
+        if name.startswith("gitter_"):
+            display = "@"+name[len("gitter_"):]
+            return display, "GitterUser"
+        if name.startswith("freenode_"):
+            display = name[len("freenode_"):]
+            return display, "FreenodeUser"
+        if name == self.user:
+            return name, "SelfUser"
+        return name, "MatrixUser"
+
     def on_message(self, room, event):
         if event['type'] == "m.room.member":
             if event['membership'] == "join":
                 self.buf_write("{0} joined".format(event['content']['displayname']))
         elif event['type'] == "m.room.message":
             if event['content']['msgtype'] == "m.text":
-                self.buf_write("{0}: {1}".format(event['sender'], event['content']['body'])+"\n")
+                name, hl = self.format_sender(event['sender'])
+                line = self.buf_write("{0}: {1}".format(name, event['content']['body']))
+                if hl:
+                    self.buf.add_highlight(hl, line, 0, len(name))
             elif event['content']['msgtype'] == "m.emote":
-                self.buf_write("* {0} {1}".format(event['sender'], event['content']['body'])+"\n")
+                self.buf_write("* {0} {1}".format(event['sender'], event['content']['body']))
             else:
                 self.buf_write("X " + event['content']['msgtype'])
         else:
@@ -60,13 +102,16 @@ class IPythonPlugin(object):
         self.create_outbuf()
         self.client = MatrixClient("https://matrix.org")
         user = self.vim.vars["matrix_user"]
+        self.user = user
         pw = self.vim.vars["matrix_passwd"]
 
         token = self.client.login_with_password(username=user, password=pw)
         self.room = self.client.join_room(self.vim.vars["matrix_room"])
 
-        self.room.add_listener(partial(self.vim.async_call, self.on_message))
+        self.room.add_listener(partial(self.vim.async_call, ExclusiveHandler(self.on_message)))
         self.client.start_listener_thread()
+        self.members = self.room.get_joined_members()
+        self.room.backfill_previous_messages(limit=50)
 
     @neovim.function("MatrixSend", sync=True)
     def matrix_send(self, args):
